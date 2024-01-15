8a550bd5497bb258df38d865328bf6321ceacce2
Lilian <lily.calvettym@gmail.com>
2024-01-15 15:37:36 -0400

    Cambios 15-01-24

--- DinoM/P_Principal.designer.vb
+++ DinoM/P_Principal.designer.vb
@@ -530,7 +530,7 @@ Partial Class P_Principal
         Me.lbVersion.SingleLineColor = System.Drawing.Color.Transparent
         Me.lbVersion.Size = New System.Drawing.Size(127, 18)
         Me.lbVersion.TabIndex = 5
-        Me.lbVersion.Text = "Versión 4.0.0.240112"
+        Me.lbVersion.Text = "Versión 4.0.0.240115"
         Me.lbVersion.TextAlignment = System.Drawing.StringAlignment.Center
         '
         'SideNav1
